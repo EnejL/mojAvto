80c3026e7d86df42573cb4a92d6693bea734fafa
EnejLicina <enej.licina@gamanza.com>
2025-11-29 15:10:07 +0100

    statCard font size change

--- screens/functionality/myVehiclesScreen.js
+++ screens/functionality/myVehiclesScreen.js
@@ -607,7 +607,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   statValue: {
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: "bold",
     color: "#2e7d32",
     marginBottom: 4,
